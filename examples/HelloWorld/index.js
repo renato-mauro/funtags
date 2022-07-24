fbcd69981d4d25b56feb7e78ad89d062d39b44c1
renato-mauro <renato.mauro@gmail.com>
2022-07-24 10:54:43 -0300

    First Commit

---
+++ examples/HelloWorld/index.js
@@ -0,0 +1,18 @@
+import { ftags } from "../../lib/ftags.js";
+
+function sayHello() {
+    /* "desconstruct" from ftags.html tags used by this template */
+    const { div, h1, p } = ftags.html;
+
+    /* create dom hierarchy by composing function call */
+    return div(
+        h1("Hello World"),
+        p("Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
+        p("Donec a nunc mi."),
+        p("Vestibulum at felis tempus, suscipit nisi in, semper nisl.")
+    );
+}
+
+window.addEventListener("load",ev=>{
+    document.body.replaceChildren(sayHello());
+});
